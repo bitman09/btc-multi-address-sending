67c76d985ca8e37e823bc40b67b35f427d704104
bitman310 <eugerman@gmx.com>
2024-11-20 03:47:42 -0600

    add new project for bitcoin/ordinals multiple address sending frontend

---
+++ app/utils/wallet/WIFWallet.ts
@@ -0,0 +1,66 @@
+import * as bitcoin from "bitcoinjs-lib";
+import { initEccLib, networks } from "bitcoinjs-lib";
+import ecc from "@bitcoinerlab/secp256k1";
+import BIP32Factory, { type BIP32Interface } from "bip32";
+import ECPairFactory, { type ECPairInterface } from "ecpair";
+
+interface IWIFWallet {
+  networkType: string;
+  privateKey: string;
+}
+
+const TESTNET = "testnet";
+
+initEccLib(ecc);
+
+const ECPair = ECPairFactory(ecc);
+const bip32 = BIP32Factory(ecc);
+
+export class WIFWallet {
+  private network: bitcoin.networks.Network;
+  public ecPair: ECPairInterface;
+  public address: string;
+  public output: Buffer;
+  public publicKey: string;
+  public seckey?: any;
+  public secret: any;
+  public pubkey: any;
+
+  constructor(walletParam: IWIFWallet) {
+    if (walletParam.networkType == TESTNET) {
+      this.network = networks.testnet;
+    } else {
+      this.network = networks.bitcoin;
+    }
+
+    this.ecPair = ECPair.fromWIF(walletParam.privateKey, this.network);
+
+    this.secret = this.ecPair.privateKey?.toString("hex");
+    // Extract the private key in hexadecimal format
+    this.secret = this.ecPair.toWIF();
+
+    // Extract the public key in hexadecimal format
+    this.pubkey = this.ecPair.publicKey.toString("hex");
+
+    const { address, output } = bitcoin.payments.p2tr({
+      internalPubkey: this.ecPair.publicKey.subarray(1, 33),
+      network: this.network,
+    });
+    this.address = address as string;
+    this.output = output as Buffer;
+    this.publicKey = this.ecPair.publicKey.toString("hex");
+  }
+
+  signPsbt(psbt: bitcoin.Psbt, ecPair: ECPairInterface): bitcoin.Psbt {
+    const tweakedChildNode = ecPair.tweak(
+      bitcoin.crypto.taggedHash("TapTweak", ecPair.publicKey.subarray(1, 33))
+    );
+
+    for (let i = 0; i < psbt.inputCount; i++) {
+      psbt.signInput(i, tweakedChildNode);
+      psbt.validateSignaturesOfInput(i, () => true);
+      psbt.finalizeInput(i);
+    }
+    return psbt;
+  }
+}
